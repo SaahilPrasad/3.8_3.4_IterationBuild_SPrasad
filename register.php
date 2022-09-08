52e7fb9d651f7b68d1494b2c4979ff9ba0c91c5e
Saahil Prasad <88806351+SaahilPrasad@users.noreply.github.com>
2022-09-08 20:37:21 +1200

    Update register.php

--- register.php
+++ register.php
@@ -17,7 +17,7 @@
 		<!-- IMPORTANT as I am using vw for responsive design -->
 		<meta name="Viewport" content="width=device-width, inital-scale=1">
 		
-		<!-- Link to style sheet for login_v1 page -->
+		<!-- Link to style sheet -->
 		<link rel="stylesheet" href="css/style_login.css"/>
 	</head>
 	
@@ -41,24 +41,23 @@
 				
 					<b><p style = "color: black; font-size: 2vw; margin: 1vw 0vw 0vw 1vw;">Register</p></b>
 					
+					<!-- Form for input fields -->
 					<h3><form method = "post" id= "01_login">
 						
-						<p style = "margin: 2.3vw 0vw 0vw 3.7vw; font-size: 1.4vw;">Username:</p>
-						<input style = "width: 18.8vw; height: 1.9vw; margin: 1vw 2vw 0vw 3.7vw;" type = "text" name = "username" placeholder="Your Username..."/>
+						<input style = "width: 18.8vw; height: 1.9vw; margin: 3.9vw 0vw 0vw 4.2vw;" type = "text" name = "username" placeholder="Username..."/>
 						
-						<p style = "margin: 1.5vw 0vw 0vw 3.7vw; font-size: 1.4vw;">Password:</p>
-						<input style = "width: 18.8vw; height: 1.9vw; margin: 1vw 2vw 0vw 3.7vw; font-size: 1.2vw; border: none;" type = "password" name = "password" placeholder="Your Password..." id="myInput"/>
+						<input style = "width: 18.8vw; height: 1.9vw; margin: 3.05vw 0vw 0vw 4.2vw;" type = "password" name = "password" placeholder="Password..." id="myInput"/>
 						
-						<p style = "margin: 1.3vw 0vw -1.3vw 5.3vw; font-size: 1vw;">Show Password</p>
+						<p style = "margin: 1.4vw 0vw -1.3vw 5.7vw; font-size: 1vw;">Show Password</p>
 						
 						<!-- Checkbox for password visibility -->
 						<input type="checkbox" onclick="myFunction()"/>
 						
-						<input style = "width: 19.2vw; height: 2.5vw; margin: 2.8vw 2vw 0vw 3.7vw; font-size: 1.2vw; border: none; border-radius: 2vw;" type = "submit" value = "Register"/>
+						<input style = "width: 19.6vw; height: 2.5vw; margin: 2.97vw 0vw 0vw 4.2vw;" type = "submit" value = "Register"/>
 					
 					</form></h3>
 					
-					<center><p style = "margin: -6vw 0vw 0vw 0vw; font-size: 1vw;">Return Back To<a href = "login_v2.html"> Login</a></p></center>
+					<center><p style = "margin: -6.5vw 0vw 0vw 0vw; font-size: 1vw;">Return Back To<a href = "login.html"> Login</a></p></center>
 					
 				</div>
 	
